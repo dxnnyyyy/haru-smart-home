d49cf4aea9fd8eb8f4ac2fe91b8d945564f53703
Daniel Koch <danielkoch@dnlkch.de>
2023-02-21 21:20:28 +0100

    cleaned up haru-python-app

--- app/haru-smart-home.py
+++ app/haru-smart-home.py
@@ -1,8 +1,11 @@
 '''
-Haru Smart Home Application :D
+Haru Smart Home Application
 '''
 import tkinter as tk
 import roslibpy as ros
+import requests as req
+from dotenv import load_dotenv
+import os
 
 class HaruSmartHome(tk.Tk):
     '''
@@ -10,9 +13,13 @@ class HaruSmartHome(tk.Tk):
     '''
     def __init__(self):
         super().__init__()
+        
+        self.dotenv = load_dotenv()
 
         self.title('Haru Smart Home Application')
         self.geometry('500x200')
+        self.hass_url = "http://localhost:8123/api/services/scene/turn_on"
+        self.hass_headers = {"Authorization": os.environ.get('hass_auth')}
 
         try:
             self.haru = ros.Ros(host='harumanager.local', port=9090)
@@ -41,7 +48,15 @@ class HaruSmartHome(tk.Tk):
         execute_scene_button = tk.Button(
             container,
             text='Start Learning',
-            command= self.start_learning
+            command=self.start_learning
+        )
+        execute_scene_button.pack()
+        
+        # Stop learning environment
+        execute_scene_button = tk.Button(
+            container,
+            text='Stop Learning',
+            command=self.stop_learning
         )
         execute_scene_button.pack()
 
@@ -57,13 +72,25 @@ class HaruSmartHome(tk.Tk):
         execute_routine_request = ros.ServiceRequest({'routine': number})
         execute_routine_service.call(execute_routine_request)
 
-        # execute some rest api call to home assistant
-
     def start_learning(self):
         '''
         Start learning environment
         '''
-        print('start learning environment')
+        res = req.post(self.hass_url, headers=self.hass_headers, json={"entity_id": "scene.haru"} )
+        if(res.status_code != 200):
+            print('Error: ', res.status_code)
+        else:
+            print('starting learning environment')
+
+    def stop_learning(self):
+        '''
+        Stop learning environment
+        '''
+        res = req.post(self.hass_url, headers=self.hass_headers, json={"entity_id": "scene.haru_off"} )
+        if(res.status_code != 200):
+            print('Error: ', res.status_code)
+        else:
+            print('stoping learning environment')
 
 
 if __name__ == '__main__':
